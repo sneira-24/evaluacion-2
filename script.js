d508575ac297c12151d9684c335cb2f91f5f10f3
Sebastian Neira <neiragavilan@gmail.com>
2026-05-27 01:40:51 -0400

    feat: eliminar usuarios

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-let btn_registro = document.getElementById("boton-registrarse");
-let usuarios = [];
-let contenedor_tabla = document.getElementById("contenedor-tabla-empleados")
+//Funcionalidad de Registro
 
-function usuarioALista(usuarios){
+let btn_registro = document.getElementById("boton-registrarse");
+let contenedor_tabla = document.getElementById("contenedor-tabla-empleados")
+let usuarios = []; //Array donde se guardarán los objetos(usuarios)
+
+function usuarioALista(usuarios){ //Función que convertirá el objeto de usuario a formato table por cada atributo
     const tabla_usuarios = document.getElementById("cuerpo-tabla")
     let ingreso_lista = ""
-    let contador = 1
+    let id = 1
     for (const usuario of usuarios){
-        ingreso_lista += `<tr>
-                            <td>${contador}</td>
-                            <td>${usuario.nombre}</td>
-                            <td>${usuario.apellido}</td>
-                            <td>${usuario.cargo}</td>
-                            <td>${usuario.email}</td>
-                            <td><button>Eliminar</button></td>
-                        </tr>`
-        contador ++;
+        if (usuario !== "") { //La razón del if se explica en la funcionalidad de eliminar usuario
+            ingreso_lista += `<tr>
+                                <td>${id}</td>
+                                <td>${usuario.nombre}</td>
+                                <td>${usuario.apellido}</td>
+                                <td>${usuario.cargo}</td>
+                                <td>${usuario.email}</td>
+                                <td><button class="btn-eliminar" onclick="eliminarUsuario(${id})">Eliminar</button></td>
+                            </tr>`
+        } 
+        id++;
     }
     tabla_usuarios.innerHTML = ingreso_lista
 }
 
-function mostrarTabla(){
+//Renderizar tabla
+function mostrarTabla(){ //Al crearse la pagina, la tabla se encuentra con display: none(oculta). Al hacerse el primer registro, la tabla se muestra.
     const tabla = document.querySelector(".contenedor")
     tabla.style.display = "block"
 }
 
-btn_registro.addEventListener("click", e =>{
+btn_registro.addEventListener("click", e =>{ //Se reciben los datos de los input al hacer click en registrar.
     e.preventDefault()
     let nombre = document.getElementById("nombre")
     nombre = nombre.value.trim()
@@ -37,8 +42,18 @@ btn_registro.addEventListener("click", e =>{
     email = email.value.trim()
 
     usuarios.push({nombre, apellido, cargo, email})
-    usuarioALista(usuarios)
+    usuarioALista(usuarios) 
     mostrarTabla()
 })
+
+
+//Funcionalidad eliminar usuario
+
+function eliminarUsuario(id) {
+    usuarios[id-1] = "" //Se reemplaza el usuario eliminado por un espacio vacio en el arreglo de objetos, de esa manera los IDs no se modifican ya que.
+    usuarioALista(usuarios) //el objeto previo aun "existe", solo que sin datos(no es representado en la tabla gracias al if en la funcion usuarioALista).
+}
+
+
 
 
